1ca3235aea01ff521993c35befb34ea6a4e5b458
htrant <trunghieu.tran138@gmail.com>
2016-10-10 14:00:47 +0300

    reply function to user, limit to max 3 events

--- functions/sender/helsinki/handler.js
+++ functions/sender/helsinki/handler.js
@@ -1,11 +1,53 @@
 'use strict';
 
 const axios = require('axios');
+var striptags = require('striptags');
 const fbGraphApi = process.env.FB_GRAPH_API;
 const helApi = process.env.HKI_LINKEDEVENT_API;
 
 
-module.exports.handler = (event, context, callback) => {
-  const reply = `List of events:`;
+const replyUser = data => {
+  const name = (data[i].name.en === undefined) ? data[i].name.fi : data[i].name.en;
+  const description = (data[i].description.en === undefined) ? data[i].description.fi : data[i].description.en;
+  let infoUrl;
+  if (data[i].info_url !== null) {
+    infoUrl = (data[i].info_url.en !== undefined) ? data[i].info_url.en : 'n/a';
+  } else {
+    infoUrl = 'n/a';
+  }
+  const replyMsg = `${i + 1}. ${name}: ${description}. More: ${infoUrl}\n`;
+  resPayload.message.text = striptags(replyMsg);
+  return axios.post(fbGraphApi, resPayload);
+};
 
+
+module.exports.handler = (event, context, callback) => {
+  const time = event.time;
+  const eventType = event.type;
+  const resPayload = event.payload;
+  const queryApi = `${helApi}&q=${eventType}&start=${time}`;
+  axios.get(queryApi)
+    .then(response => {
+      const data = response.data.data;
+      const max = (data.length < 3) ? data.length : 3;
+      let replyMsg = 'List of events:\n';
+      for (let i = 0; i < max; i++) {
+        const name = (data[i].name.en === undefined) ? data[i].name.fi : data[i].name.en;
+        // const description = (data[i].description.en === undefined) ? data[i].description.fi : data[i].description.en;
+        let infoUrl;
+        if (data[i].info_url !== null) {
+          infoUrl = (data[i].info_url.en !== undefined) ? data[i].info_url.en : 'n/a';
+        } else {
+          infoUrl = 'n/a';
+        }
+        // replyMsg += `${i + 1}. ${name}: ${description}. More: ${infoUrl}\n`; //with description
+        replyMsg += `${i + 1}. ${name}. More: ${infoUrl}\n`;
+      }
+      resPayload.message.text = striptags(replyMsg);
+      return axios.post(fbGraphApi, resPayload);
+    })
+    .catch(error => {
+      axios.post(fbGraphApi, resPayload);
+      callback(error);
+    });
 };
