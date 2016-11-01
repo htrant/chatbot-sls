48bd4d9b14e838fc206f4d635eaeb022578309a3
Hieu Tran <trunghieu.tran138@gmail.com>
2016-11-01 23:47:49 +0200

    added event rate, sent 3 replies each time function is invoked

--- functions/test/benchmark/handler.js
+++ functions/test/benchmark/handler.js
@@ -7,45 +7,48 @@ const cities = ['espoo', 'helsinki', 'turku'];
 const fbUsers = [process.env.USER1_ID, process.env.USER2_ID];
 
 
+// once function is invoked, 3 various replies are sent to FB users
 module.exports.handler = (event, context) => {
-  const msgCallback = {
-    object: 'page',
-    entry: []
-  };
-  const epochTime = new Date().valueOf();
-  let msgText = `${_.sample(cities)} ${_.sample(eventKeywords)}`;
-  if ((epochTime % 2) === 0) {
-    msgText = `${msgText} today`;
-  } else {
-    const today = new Date().toISOString().slice(0, 10);
-    msgText = `${msgText} ${today} 2016-12-31`;
-  }
-  const entryDetail = {
-    id: process.env.PAGE_ID,
-    messaging: [
-      {
-        sender: {
-          id: _.sample(fbUsers)
-        },
-        recipient: {
-          id: process.env.PAGE_ID
-        },
-        timestamp: epochTime,
-        message: {
-          text: msgText
+  for (let i = 0; i < 2; i++) {
+    const msgCallback = {
+      object: 'page',
+      entry: []
+    };
+    const epochTime = new Date().valueOf();
+    let msgText = `${_.sample(cities)} ${_.sample(eventKeywords)}`;
+    if ((epochTime % 2) === 0) {
+      msgText = `${msgText} today`;
+    } else {
+      const today = new Date().toISOString().slice(0, 10);
+      msgText = `${msgText} ${today} 2016-12-31`;
+    }
+    const entryDetail = {
+      id: process.env.PAGE_ID,
+      messaging: [
+        {
+          sender: {
+            id: _.sample(fbUsers)
+          },
+          recipient: {
+            id: process.env.PAGE_ID
+          },
+          timestamp: epochTime,
+          message: {
+            text: msgText
+          }
         }
-      }
-    ]
-  };
-  msgCallback.entry.push(entryDetail);
-  console.info('msgCallback', JSON.stringify(msgCallback, null, 2));
-  console.info('receiver endpoint', process.env.APIGATEWAY_RECEIVER);
-  axios.post(process.env.APIGATEWAY_RECEIVER, msgCallback)
-    .then(res => {
-      console.info('success', res);
-      context.done(null);
-    })
-    .catch(err => {
-      console.error('error', err);
-    })
+      ]
+    };
+    msgCallback.entry.push(entryDetail);
+    console.info('msgCallback', JSON.stringify(msgCallback, null, 2));
+    console.info('receiver endpoint', process.env.APIGATEWAY_RECEIVER);
+    axios.post(process.env.APIGATEWAY_RECEIVER, msgCallback)
+      .then(res => {
+        console.info('success', res);
+        context.done(null);
+      })
+      .catch(err => {
+        console.error('error', err);
+      })
+  }
 };
