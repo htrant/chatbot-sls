320e22de862531d5f6221d3277396e101d0a9d56
htrant <trunghieu.tran138@gmail.com>
2016-11-18 23:37:05 +0200

    completed to find event for new ui

--- functions/receiver-new/handler.js
+++ functions/receiver-new/handler.js
@@ -2,6 +2,12 @@
 
 const axios = require('axios');
 const fbGraphApi = process.env.FB_GRAPH_API;
+const moment = require('moment');
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda({
+  apiVersion: '2015-03-31',
+  region: process.env.SERVERLESS_REGION
+});
 
 
 function prepareCityPicker(usrId) {
@@ -62,8 +68,8 @@ function prepareEventPicker(usrId, city) {
             },
             {
               type: 'postback',
-              title: 'Theatre',
-              payload: `{"city":"${city}","event":"theatre"}`
+              title: 'Christmas',
+              payload: `{"city":"${city}","event":"christmas"}`
             }
           ]
         }
@@ -73,6 +79,8 @@ function prepareEventPicker(usrId, city) {
 }
 
 function prepareDatePicker(usrId, city, event) {
+  const tmr = moment().add(1, 'days').format('YYYY-MM-DD');
+  const next3days = moment().add(3, 'days').format('YYYY-MM-DD');
   return {
     recipient: {
       id: usrId
@@ -85,12 +93,19 @@ function prepareDatePicker(usrId, city, event) {
           text: `Which date in ${city} do you look for ${event} events?`,
           buttons: [
             {
-              type: 'web_url',
-              url: process.env.DATE_PICKER_URL,
-              title: 'Select a date',
-              webview_height_ratio: 'compact',
-              messenger_extensions: true,
-              fallback_url: ''
+              type: 'postback',
+              title: 'Today',
+              payload: `{"city":"${city}","type":"${event}","time":"today"}`
+            },
+            {
+              type: 'postback',
+              title: 'Tomorrow',
+              payload: `{"city":"${city}","type":"${event}","time":"${tmr}"}`
+            },
+            {
+              type: 'postback',
+              title: 'Next 3 days',
+              payload: `{"city":"${city}","type":"${event}","time":"${next3days}"}`
             }
           ]
         }
@@ -99,10 +114,32 @@ function prepareDatePicker(usrId, city, event) {
   };
 }
 
+function invokeLambda(payload) {
+  console.info('call lambda function sender processor');
+  return new Promise((resolve, reject) => {
+    const params = {
+      FunctionName: process.env.LAMBDA_SENDER_PROCESSOR,
+      Payload: JSON.stringify(payload, null, 2),
+      Qualifier: process.env.SERVERLESS_STAGE
+    };
+    lambda.invoke(params, (err, data) => {
+      if (err) {
+        console.error('failure:', JSON.stringify(err));
+        reject({
+          sender: payload.recipient.id,
+          reason: 'Sorry, we have some technical problems'
+        });
+      } else {
+        console.info('success:', JSON.stringify(data));
+        resolve(data);
+      }
+    });
+  });
+}
+
 
 module.exports.handler = (event, context, callback) => {
   console.info('invoke lambda function receiver-new');
-  console.info('event:', JSON.stringify(event, null, 2));
 
   // verify fb app token
   if (event.method === 'GET') {
@@ -128,13 +165,13 @@ module.exports.handler = (event, context, callback) => {
     } else if (msgItem.hasOwnProperty('postback')) {
       const payload = JSON.parse(msgItem.postback.payload);
       const event = prepareEventPicker(msgItem.sender.id, payload.city);
-      if (!payload.hasOwnProperty('event') && !payload.hasOwnProperty('date')) {
+      if (!payload.hasOwnProperty('event') && !payload.hasOwnProperty('time')) {
         const event = prepareEventPicker(msgItem.sender.id, payload.city);
         axios.post(fbGraphApi, event)
           .then(error => {
             console.error('FB send error:', JSON.stringify(error));
           });
-      } else if (payload.hasOwnProperty('event') && !payload.hasOwnProperty('date')) {
+      } else if (payload.hasOwnProperty('event') && !payload.hasOwnProperty('time')) {
         const date = prepareDatePicker(msgItem.sender.id, payload.city, payload.event);
         console.info('datePicker', JSON.stringify(date, null, 2));
         axios.post(fbGraphApi, date)
@@ -142,7 +179,35 @@ module.exports.handler = (event, context, callback) => {
             console.error('FB send error:', JSON.stringify(error));
           });
       } else {
-        console.info('collect enough information');
+        console.info('enough information, start to search for events');
+        const lambdaPayload = payload;
+        lambdaPayload.payload = {
+          recipient: {
+            id: msgItem.sender.id
+          },
+          message: {
+            text: ''
+          }
+        };
+        console.info('lambda payload:', JSON.stringify(lambdaPayload));
+        invokeLambda(lambdaPayload)
+          .catch(error => {
+            console.error('failure', JSON.stringify(error, null, 2));
+            const payload = {
+              recipient: {
+                id: error.sender
+              },
+              message: {
+                text: error.reason
+              }
+            };
+            console.info(`FB api: ${fbGraphApi}`);
+            console.info('FB send payload:', JSON.stringify(payload));
+            axios.post(fbGraphApi, payload)
+              .catch(error => {
+                console.error('FB send error:', JSON.stringify(error));
+              });
+          });
       }
     }
   }
